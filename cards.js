6d52a5dc00537e80acb85086812e85c2fa1920b9
Sam Jonston <johnstonsam712@gmail.com>
2024-09-22 17:29:43 +0100

    array with all properties

---
+++ cards.js
@@ -0,0 +1,180 @@
+export const PROPERTY_CARDS = [
+  //BROWNS
+  {
+    type: 'property',
+    value: 1,
+    color: 'brown',
+    name: 'Old Kent Road'
+  },
+  {
+    type: 'property',
+    value: 1,
+    color: 'brown',
+    name: 'Whitechapel Road'
+  },
+  //LIGHT BLUES
+  {
+    type: 'property',
+    value: 1,
+    color: 'light blue',
+    name: 'Euston Road'
+  },
+  {
+    type: 'property',
+    value: 1,
+    color: 'light blue',
+    name: 'Pentonville Road'
+  },
+  {
+    type: 'property',
+    value: 1,
+    color: 'light blue',
+    name: 'The Anglel, Islington'
+  },
+  //MAGENTAS
+  {
+    type: 'property',
+    value: 2,
+    color: 'magenta',
+    name: 'Whitehall'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'magenta',
+    name: 'Pall Mall'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'magenta',
+    name: 'Northumberland Avenue'
+  },
+  //ORANGES
+  {
+    type: 'property',
+    value: 2,
+    color: 'orange',
+    name: 'Marlborough Street'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'orange',
+    name: 'Bow Street'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'orange',
+    name: 'Vine Street'
+  },
+  //REDS
+  {
+    type: 'property',
+    value: 3,
+    color: '#D02F26',
+    name: 'Trafalgar Square'
+  },
+  {
+    type: 'property',
+    value: 3,
+    color: '#D02F26',
+    name: 'Fleet Street'
+  },
+  {
+    type: 'property',
+    value: 3,
+    color: '#D02F26',
+    name: 'Strand'
+  },
+  //YELLOWS
+  {
+    type: 'property',
+    value: 3,
+    color: 'yellow',
+    name: 'Piccadilly'
+  },
+  {
+    type: 'property',
+    value: 3,
+    color: 'yellow',
+    name: 'Leicester Square'
+  },
+  {
+    type: 'property',
+    value: 3,
+    color: 'yellow',
+    name: 'Coventry Street'
+  },
+  //GREENS
+  {
+    type: 'property',
+    value: 4,
+    color: 'green',
+    name: 'Bond Street'
+  },
+  {
+    type: 'property',
+    value: 4,
+    color: 'green',
+    name: 'Oxford Street'
+  },
+  {
+    type: 'property',
+    value: 4,
+    color: 'green',
+    name: 'Regent Street'
+  },
+  //DARK BLUES
+  {
+    type: 'property',
+    value: 4,
+    color: 'dark blue',
+    name: 'Park Lane'
+  },
+  {
+    type: 'property',
+    value: 4,
+    color: 'dark blue',
+    name: 'Mayfair'
+  },
+  //STATIONS
+  {
+    type: 'property',
+    value: 2,
+    color: 'black',
+    name: 'Fenchurch Street Station'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'black',
+    name: 'Liverpool Street Station'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'black',
+    name: 'Kings Cross Station'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'black',
+    name: 'Marylebone Station'
+  },
+  //UTILITIES
+  {
+    type: 'property',
+    value: 2,
+    color: 'teal',
+    name: 'Water Works'
+  },
+  {
+    type: 'property',
+    value: 2,
+    color: 'teal',
+    name: 'Electric Company'
+  }
+]
