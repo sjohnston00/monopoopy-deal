b536a3e1a66ac489ac64a7a0cf589ae0762e16bd
Sam Jonston <johnstonsam712@gmail.com>
2024-12-28 21:16:48 +0000

    refactor colours to file

--- cards.js
+++ cards.js
@@ -1,180 +1,182 @@
+import { COLOURS } from './colours.js'
+
 export const PROPERTY_CARDS = [
   //BROWNS
   {
     type: 'property',
     value: 1,
-    color: 'brown',
+    color: COLOURS.SADDLE_BROWN,
     name: 'Old Kent Road'
   },
   {
     type: 'property',
     value: 1,
-    color: 'brown',
+    color: COLOURS.SADDLE_BROWN,
     name: 'Whitechapel Road'
   },
   //LIGHT BLUES
   {
     type: 'property',
     value: 1,
-    color: 'light blue',
+    color: COLOURS.SKY_BLUE,
     name: 'Euston Road'
   },
   {
     type: 'property',
     value: 1,
-    color: 'light blue',
+    color: COLOURS.SKY_BLUE,
     name: 'Pentonville Road'
   },
   {
     type: 'property',
     value: 1,
-    color: 'light blue',
+    color: COLOURS.SKY_BLUE,
     name: 'The Anglel, Islington'
   },
   //MAGENTAS
   {
     type: 'property',
     value: 2,
-    color: 'magenta',
+    color: COLOURS.DARK_ORCHID,
     name: 'Whitehall'
   },
   {
     type: 'property',
     value: 2,
-    color: 'magenta',
+    color: COLOURS.DARK_ORCHID,
     name: 'Pall Mall'
   },
   {
     type: 'property',
     value: 2,
-    color: 'magenta',
+    color: COLOURS.DARK_ORCHID,
     name: 'Northumberland Avenue'
   },
   //ORANGES
   {
     type: 'property',
     value: 2,
-    color: 'orange',
+    color: COLOURS.ORANGE,
     name: 'Marlborough Street'
   },
   {
     type: 'property',
     value: 2,
-    color: 'orange',
+    color: COLOURS.ORANGE,
     name: 'Bow Street'
   },
   {
     type: 'property',
     value: 2,
-    color: 'orange',
+    color: COLOURS.ORANGE,
     name: 'Vine Street'
   },
   //REDS
   {
     type: 'property',
     value: 3,
-    color: '#D02F26',
+    color: COLOURS.RED,
     name: 'Trafalgar Square'
   },
   {
     type: 'property',
     value: 3,
-    color: '#D02F26',
+    color: COLOURS.RED,
     name: 'Fleet Street'
   },
   {
     type: 'property',
     value: 3,
-    color: '#D02F26',
+    color: COLOURS.RED,
     name: 'Strand'
   },
   //YELLOWS
   {
     type: 'property',
     value: 3,
-    color: 'yellow',
+    color: COLOURS.YELLOW,
     name: 'Piccadilly'
   },
   {
     type: 'property',
     value: 3,
-    color: 'yellow',
+    color: COLOURS.YELLOW,
     name: 'Leicester Square'
   },
   {
     type: 'property',
     value: 3,
-    color: 'yellow',
+    color: COLOURS.YELLOW,
     name: 'Coventry Street'
   },
   //GREENS
   {
     type: 'property',
     value: 4,
-    color: 'green',
+    color: COLOURS.GREEN,
     name: 'Bond Street'
   },
   {
     type: 'property',
     value: 4,
-    color: 'green',
+    color: COLOURS.GREEN,
     name: 'Oxford Street'
   },
   {
     type: 'property',
     value: 4,
-    color: 'green',
+    color: COLOURS.GREEN,
     name: 'Regent Street'
   },
   //DARK BLUES
   {
     type: 'property',
     value: 4,
-    color: 'dark blue',
+    color: COLOURS.BLUE,
     name: 'Park Lane'
   },
   {
     type: 'property',
     value: 4,
-    color: 'dark blue',
+    color: COLOURS.BLUE,
     name: 'Mayfair'
   },
   //STATIONS
   {
     type: 'property',
     value: 2,
-    color: 'black',
+    color: COLOURS.BLACK,
     name: 'Fenchurch Street Station'
   },
   {
     type: 'property',
     value: 2,
-    color: 'black',
+    color: COLOURS.BLACK,
     name: 'Liverpool Street Station'
   },
   {
     type: 'property',
     value: 2,
-    color: 'black',
+    color: COLOURS.BLACK,
     name: 'Kings Cross Station'
   },
   {
     type: 'property',
     value: 2,
-    color: 'black',
+    color: COLOURS.BLACK,
     name: 'Marylebone Station'
   },
   //UTILITIES
   {
     type: 'property',
     value: 2,
-    color: 'teal',
+    color: COLOURS.TEAL,
     name: 'Water Works'
   },
   {
     type: 'property',
     value: 2,
-    color: 'teal',
+    color: COLOURS.TEAL,
     name: 'Electric Company'
   }
 ]
---
+++ colours.js
@@ -0,0 +1,13 @@
+export const COLOURS = {
+  SADDLE_BROWN: 'SaddleBrown',
+  SKY_BLUE: 'SkyBlue',
+  DARK_ORCHID: 'DarkOrchid',
+  ORANGE: 'Orange',
+  // RED: 'Red',
+  RED: '#D02F26',
+  YELLOW: 'Yellow',
+  GREEN: 'Green',
+  BLUE: 'Blue',
+  BLACK: 'Black',
+  TEAL: 'Teal'
+}
