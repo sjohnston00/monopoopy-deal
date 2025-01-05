c87d5c6640e698aa06cb4b13aebec1a0c161807c
Sam Jonston <johnstonsam712@gmail.com>
2025-01-05 16:36:55 +0000

    add property rent values to array

--- cards.js
+++ cards.js
@@ -1,182 +1,223 @@
 import { COLOURS } from './colours.js'
 
+export const PROPERTY_CARD_RENT_VALUES = {
+  BROWNS: [1, 2],
+  BLUES: [1, 2, 3],
+  MAGENTAS: [1, 2, 4],
+  ORANGES: [1, 3, 5],
+  REDS: [2, 3, 6],
+  YELLOWS: [2, 4, 6],
+  GREENS: [2, 4, 7],
+  DARK_BLUES: [3, 8],
+  STATIONS: [1, 2, 3, 4],
+  UTILITIES: [1, 2]
+}
+
 export const PROPERTY_CARDS = [
   //BROWNS
   {
     type: 'property',
     value: 1,
     color: COLOURS.SADDLE_BROWN,
-    name: 'Old Kent Road'
+    name: 'Old Kent Road',
+    rent: PROPERTY_CARD_RENT_VALUES.BROWNS
   },
   {
     type: 'property',
     value: 1,
     color: COLOURS.SADDLE_BROWN,
-    name: 'Whitechapel Road'
+    name: 'Whitechapel Road',
+    rent: PROPERTY_CARD_RENT_VALUES.BROWNS
   },
   //LIGHT BLUES
   {
     type: 'property',
     value: 1,
     color: COLOURS.SKY_BLUE,
-    name: 'Euston Road'
+    name: 'Euston Road',
+    rent: PROPERTY_CARD_RENT_VALUES.BLUES
   },
   {
     type: 'property',
     value: 1,
     color: COLOURS.SKY_BLUE,
-    name: 'Pentonville Road'
+    name: 'Pentonville Road',
+    rent: PROPERTY_CARD_RENT_VALUES.BLUES
   },
   {
     type: 'property',
     value: 1,
     color: COLOURS.SKY_BLUE,
-    name: 'The Anglel, Islington'
+    name: 'The Anglel, Islington',
+    rent: PROPERTY_CARD_RENT_VALUES.BLUES
   },
   //MAGENTAS
   {
     type: 'property',
     value: 2,
     color: COLOURS.DARK_ORCHID,
-    name: 'Whitehall'
+    name: 'Whitehall',
+    rent: PROPERTY_CARD_RENT_VALUES.MAGENTAS
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.DARK_ORCHID,
-    name: 'Pall Mall'
+    name: 'Pall Mall',
+    rent: PROPERTY_CARD_RENT_VALUES.MAGENTAS
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.DARK_ORCHID,
-    name: 'Northumberland Avenue'
+    name: 'Northumberland Avenue',
+    rent: PROPERTY_CARD_RENT_VALUES.MAGENTAS
   },
   //ORANGES
   {
     type: 'property',
     value: 2,
     color: COLOURS.ORANGE,
-    name: 'Marlborough Street'
+    name: 'Marlborough Street',
+    rent: PROPERTY_CARD_RENT_VALUES.ORANGES
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.ORANGE,
-    name: 'Bow Street'
+    name: 'Bow Street',
+    rent: PROPERTY_CARD_RENT_VALUES.ORANGES
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.ORANGE,
-    name: 'Vine Street'
+    name: 'Vine Street',
+    rent: PROPERTY_CARD_RENT_VALUES.ORANGES
   },
   //REDS
   {
     type: 'property',
     value: 3,
     color: COLOURS.RED,
-    name: 'Trafalgar Square'
+    name: 'Trafalgar Square',
+    rent: PROPERTY_CARD_RENT_VALUES.REDS
   },
   {
     type: 'property',
     value: 3,
     color: COLOURS.RED,
-    name: 'Fleet Street'
+    name: 'Fleet Street',
+    rent: PROPERTY_CARD_RENT_VALUES.REDS
   },
   {
     type: 'property',
     value: 3,
     color: COLOURS.RED,
-    name: 'Strand'
+    name: 'Strand',
+    rent: PROPERTY_CARD_RENT_VALUES.REDS
   },
   //YELLOWS
   {
     type: 'property',
     value: 3,
     color: COLOURS.YELLOW,
-    name: 'Piccadilly'
+    name: 'Piccadilly',
+    rent: PROPERTY_CARD_RENT_VALUES.YELLOWS
   },
   {
     type: 'property',
     value: 3,
     color: COLOURS.YELLOW,
-    name: 'Leicester Square'
+    name: 'Leicester Square',
+    rent: PROPERTY_CARD_RENT_VALUES.YELLOWS
   },
   {
     type: 'property',
     value: 3,
     color: COLOURS.YELLOW,
-    name: 'Coventry Street'
+    name: 'Coventry Street',
+    rent: PROPERTY_CARD_RENT_VALUES.YELLOWS
   },
   //GREENS
   {
     type: 'property',
     value: 4,
     color: COLOURS.GREEN,
-    name: 'Bond Street'
+    name: 'Bond Street',
+    rent: PROPERTY_CARD_RENT_VALUES.GREENS
   },
   {
     type: 'property',
     value: 4,
     color: COLOURS.GREEN,
-    name: 'Oxford Street'
+    name: 'Oxford Street',
+    rent: PROPERTY_CARD_RENT_VALUES.GREENS
   },
   {
     type: 'property',
     value: 4,
     color: COLOURS.GREEN,
-    name: 'Regent Street'
+    name: 'Regent Street',
+    rent: PROPERTY_CARD_RENT_VALUES.GREENS
   },
   //DARK BLUES
   {
     type: 'property',
     value: 4,
     color: COLOURS.BLUE,
-    name: 'Park Lane'
+    name: 'Park Lane',
+    rent: PROPERTY_CARD_RENT_VALUES.DARK_BLUES
   },
   {
     type: 'property',
     value: 4,
     color: COLOURS.BLUE,
-    name: 'Mayfair'
+    name: 'Mayfair',
+    rent: PROPERTY_CARD_RENT_VALUES.DARK_BLUES
   },
   //STATIONS
   {
     type: 'property',
     value: 2,
     color: COLOURS.BLACK,
-    name: 'Fenchurch Street Station'
+    name: 'Fenchurch Street Station',
+    rent: PROPERTY_CARD_RENT_VALUES.STATIONS
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.BLACK,
-    name: 'Liverpool Street Station'
+    name: 'Liverpool Street Station',
+    rent: PROPERTY_CARD_RENT_VALUES.STATIONS
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.BLACK,
-    name: 'Kings Cross Station'
+    name: 'Kings Cross Station',
+    rent: PROPERTY_CARD_RENT_VALUES.STATIONS
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.BLACK,
-    name: 'Marylebone Station'
+    name: 'Marylebone Station',
+    rent: PROPERTY_CARD_RENT_VALUES.STATIONS
   },
   //UTILITIES
   {
     type: 'property',
     value: 2,
     color: COLOURS.TEAL,
-    name: 'Water Works'
+    name: 'Water Works',
+    rent: PROPERTY_CARD_RENT_VALUES.UTILITIES
   },
   {
     type: 'property',
     value: 2,
     color: COLOURS.TEAL,
-    name: 'Electric Company'
+    name: 'Electric Company',
+    rent: PROPERTY_CARD_RENT_VALUES.UTILITIES
   }
 ]
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { PROPERTY_CARDS } from './cards.js'
+import { COLOURS } from './colours.js'
 
 const cards = [
   {
@@ -22,13 +23,20 @@ function createProperyCard(card) {
   const propertyCard = document.createElement('div')
   propertyCard.classList.add('property-card')
 
+  const cardBgColour = card.color
+  const cardColour =
+    card.color === COLOURS.YELLOW || card.color === COLOURS.SKY_BLUE
+      ? 'black'
+      : 'white'
+
   const propertyCardHeader = document.createElement('div')
   propertyCardHeader.classList.add('property-card-header')
-  propertyCardHeader.style.backgroundColor = card.color
+  propertyCardHeader.style.backgroundColor = cardBgColour
 
   const propertyCardHeaderTitle = document.createElement('span')
   propertyCardHeaderTitle.textContent = card.name
   propertyCardHeaderTitle.classList.add('property-card-header-title')
+  propertyCardHeaderTitle.style.color = cardColour
   propertyCardHeader.appendChild(propertyCardHeaderTitle)
 
   const propertyCardHeaderValueWrapper = document.createElement('div')
@@ -49,6 +57,48 @@ function createProperyCard(card) {
 
   const propertyCardBody = document.createElement('div')
   propertyCardBody.classList.add('property-card-body')
+
+  const propertyCardInfoTextRow = document.createElement('div')
+  propertyCardInfoTextRow.classList.add('property-card-body-info-text-row')
+  propertyCardInfoTextRow.innerHTML =
+    '<span>Properties<br />Owned</span><span>Rent</span>'
+
+  propertyCardBody.appendChild(propertyCardInfoTextRow)
+
+  if (card.rent) {
+    for (let index = 0; index < card.rent.length; index++) {
+      const rentValue = card.rent[index]
+      const propertyCardInfoRow = document.createElement('div')
+
+      propertyCardInfoRow.classList.add('property-card-body-info-row')
+      propertyCardInfoRow.innerHTML = `<div class="property-card-body-info-row-card">
+              <div class="property-card-body-info-row-card-header" style="background-color: ${cardBgColour}"></div>
+              <div class="property-card-body-info-row-card-body" style="color: ${
+                card.color === COLOURS.YELLOW || card.color === COLOURS.SKY_BLUE
+                  ? 'black'
+                  : cardBgColour
+              };">
+                <span>${index + 1}</span>
+              </div>
+            </div>
+            <div class="property-card-body-info-row-rent">
+              <div class="property-card-body-info-row-rent-wrapper">
+                <span class="property-card-body-info-row-rent-value-sign"
+                  >m</span
+                >
+                <span class="property-card-body-info-row-rent-value">${rentValue}</span>
+              </div>
+            </div>`
+      propertyCardBody.appendChild(propertyCardInfoRow)
+    }
+  }
+
+  const propertyCardInfoFooterRow = document.createElement('div')
+  propertyCardInfoFooterRow.classList.add('property-card-body-info-text-row')
+  propertyCardInfoFooterRow.innerHTML =
+    '<span>Complete<br />Set</span><span></span>'
+
+  propertyCardBody.appendChild(propertyCardInfoFooterRow)
 
   propertyCard.appendChild(propertyCardHeader)
   propertyCard.appendChild(propertyCardBody)
